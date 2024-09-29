2091f353279d4547d1a2c31b5aaa11a07e107016
Valentina Goncharenko <goncharenko.vp@ispras.ru>
2024-09-29 23:17:27 +0300

    02.01

---
+++ 1week/02.01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void classifySymbol(char c) {
+    switch (c) {
+        //decimal numbers
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+            std::cout << c << " - decimal number\n" << std::endl;
+            break;
+        //arithmetic operators
+        case '+': case '-': case '*': case '/':
+            std::cout << c << " - arithmetic operator\n" << std::endl;
+            break;
+        //brackets
+        case '(': case ')':
+            std::cout << c << " - bracket\n" << std::endl;
+            break;
+        //other
+        default:
+            std::cout << c << " - other\n" << std::endl;
+            break;
+    }
+}
+
+int main() {
+    std::string input;
+    
+    std::cout << "Enter the symbol: ";
+    std::cin >> input;
+
+    for (char c : input) {
+        classifySymbol(c);
+    }
+
+    return EXIT_SUCCESS;
+}
